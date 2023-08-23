4c7b874e4ef5fe35dec287baedc2b3a053cd9e69
Nicholas Barosso <nbarosso@gmail.com>
2023-08-23 10:11:08 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) { 
+        return driver.toUpperCase() === name.toUpperCase()
+    })
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase().substring(0, letters.length) === letters.toUpperCase()
+    })
+}
+
+function matchName (drivers, place) {
+    return drivers.filter(function (driver) {
+        return driver.name === place
+    })
+}
